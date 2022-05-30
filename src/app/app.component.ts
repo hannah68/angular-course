c5eea47de7cfc2adf06d1361b479eb61b8907735
hannah68 <reihaneh.naderi1989@gmail.com>
2022-05-30 13:49:52 +0100

    learn pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ import { Course } from './model/course';
 export class AppComponent {
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
+  title= COURSES[0].description;
+
+  price=9.99;
+
+  rate= 0.67;
+
+  course=COURSES[0];
+
   onCourseSelected(course: Course){
     console.log('courseSelected', course)
   }
