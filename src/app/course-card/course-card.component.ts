d1f1550c172e546ab330fbcc9905ba88cbccded6
hannah68 <reihaneh.naderi1989@gmail.com>
2022-05-30 10:10:45 +0100

    display card on screen

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import {Course} from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCourseViewed(){
+    console.log('clicked');
+    this.courseSelected.emit(this.course)
+  }
+
+}
